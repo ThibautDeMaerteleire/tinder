8622b87e84732cd991f58b6860e6e40c0009456f
Codelicious <thibdema@student.arteveldehs.be>
2020-03-18 15:10:10 +0100

    events fixed

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,11 +1,15 @@
 import { randomUser10 } from './config.js';
 import { fetchDATA } from './fetchUsers.js';
+import { like, disLike, nextUser } from './events.js';
 
-
+function allEvents() {
+  like();
+  disLike();
+}
 
 const app = () => {
   fetchDATA(randomUser10);
-
+  allEvents();
 }
 
 app();
--- src/scripts/events.js
+++ src/scripts/events.js
@@ -1,13 +1,22 @@
+const stamp = document.querySelector('#stamp');
+
 export function like() {
   const like = document.querySelector('#like');
-  const stamp = document.querySelector('#stamp');
   like.addEventListener('click', (ev) => {
-    stamp.classList.add('showStamp');
+    stamp.style.transition = '0.2s';
+    stamp.style.opacity = 1;
+    console.log('lol');
   });
 }
 
-export function dislike() {
-
+export function disLike() {
+  const dislike = document.querySelector('#dislike');
+  dislike.addEventListener('click', (ev) => {
+    stamp.setAttribute('src', './images/dislike_stamp.png')
+    stamp.style.transition = '0.2s';
+    stamp.style.opacity = 1;
+    console.log('lol');
+  });
 }
 
 export function nextUser() {
